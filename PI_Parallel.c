02440eb2824b6b791fb048b014840b330db345ec
Aarsh Bhavsar <97463622+aarshbhavsar123@users.noreply.github.com>
2024-03-16 14:19:32 +0530

    Add files via upload

--- PI_Parallel.c
+++ PI_Parallel.c
@@ -6,7 +6,7 @@ static long num_steps = 100000;
 double step;
 int main()
 {   
-    int i,nthreads;double pi,sum[NUM_THREAD][PAD];
+    int i,nthreads;double pi;
     step = 1.0/num_steps;
     omp_set_num_threads(NUM_THREAD);
     double start_time, end_time;
@@ -15,20 +15,21 @@ int main()
     {
         int i,id,nthrds;
         double x;
+        double sum = 0.0;
         id = omp_get_thread_num();
         nthrds = omp_get_num_threads();
         if(id == 0)
         {
             nthreads = nthrds;
         }
-        sum[id][0] = 0.0;
+        sum= 0.0;
         for(int i = id;i<num_steps;i+=nthrds)
         {
             x = (i)*step;
-            sum[id][0]+=(4.0)/(1.0+x*x);
+            sum+=(4.0)/(1.0+x*x);
         }
         #pragma omp critical
-        pi+=sum[id][0]*step;
+        pi+=sum*step;
     }
     end_time = omp_get_wtime();
     printf("PI = %f\n",pi);
